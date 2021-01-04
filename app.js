0a2894d831b490eb5a6909d6a1597640382a16cf
Muhammad Qadir <mahdimq@gmail.com>
2021-01-04 20:45:34 +0300

    fix bugs

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const ExpressError = require('./helpers/expressError');
 app.use(express.json());
 app.use(cors());
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	next();
+});
+
 // add logging system
 // app.use(morgan('dev'));
 app.use(morgan('tiny'));
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-/** Start server for jobly. */
-
+/** Start server for The Watchlist. */
 
 const app = require('./app');
-const { PORT } = require("./config");
-
+const { PORT } = require('./config');
 
 app.listen(PORT, function () {
-  console.log(`Server starting on port ${PORT}!`);
+	console.log(`Server starting on port ${PORT}!`);
 });
