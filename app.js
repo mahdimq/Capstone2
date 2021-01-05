ee0ef07c0e02a650877ef4117503fbd34019aa44
Muhammad Qadir <mahdimq@gmail.com>
2021-01-05 10:56:39 +0300

    fix error

--- app.js
+++ app.js
@@ -7,10 +7,29 @@ const morgan = require('morgan');
 const ExpressError = require('./helpers/expressError');
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-// const whitelist = ['http://localhost:3000', 'https://thewatchlist.netlify.app']; // list of allowed domains
-app.options('*', cors());
+const whitelist = [
+	'http://localhost:3000',
+	'https://thewatchlist.netlify.app',
+	'https://thewatchlist-backend.herokuapp.com/'
+]; // list of allowed domains
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin) {
+			return callback(null, true);
+		}
+
+		if (whitelist.indexOf(origin) === -1) {
+			var msg =
+				'The CORS policy for this site does not ' +
+				'allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	}
+};
 
 // end
 app.use(cors(corsOptions));
